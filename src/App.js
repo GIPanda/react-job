f3babf3d7ff07290eada698312cc051c307189b6
Dongjie LIU <patrickliu1989@gmail.com>
2018-06-24 23:33:28 +0200

    create init project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import  {createStore} from 'redux';
+import { SSL_OP_PKCS1_CHECK_2 } from 'constants';
 
 class App extends Component {
   render() {
+    const boss = 'gipanda';
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>Knights {boss}</h2>
+        <Team1 leader='Patrick'></Team1>
+        <Team2 leader='Frank'></Team2>
       </div>
     );
+  }
+}
+
+function Team2(props) {
+  return <h2>Team2 leader {props.leader}</h2>
+}
+
+class Team1 extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      soldiers: ['sd1', 'sd2', 'sd3']
+    }
+  }
+
+  addSoldier = () => {
+    console.log('hello add soldier');
+    this.setState({
+      soldiers: [...this.state.soldiers, 'new soldier' + Math.random()]
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Team1 leader of team one, {this.props.leader}</h2>
+        <button onClick={this.addSoldier}>add member</button>
+        <ul>
+          {this.state.soldiers.map(v => {
+            return <li key={v}>{v}</li>
+          })}
+        </ul>
+      </div>
+    )
   }
 }
 
