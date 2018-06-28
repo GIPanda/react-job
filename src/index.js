030a42677cf78b1f4187d1378b4bb1c6064dee7a
Dongjie LIU <patrickliu1989@gmail.com>
2018-06-28 23:54:51 +0200

    introduce react router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from "redux-thunk";
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Link} from 'react-router-dom';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { counter } from './index.redux';
@@ -12,9 +14,34 @@ const store = createStore(counter, compose(
   window.devToolsExtension ? window.devToolsExtension() : () => {}
 ));
 
+function Team2() {
+  return <h2>Team2</h2>
+}
+
+function Team3() {
+  return <h2>Team3</h2>
+}
+
 ReactDOM.render(
   (<Provider store={store}>
-    <App/>
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+            <Link to='/'>Team1</Link>
+          </li>
+          <li>
+            <Link to='/team2'>Team2</Link>
+          </li>
+          <li>
+            <Link to='/team3'>Team3</Link>
+          </li>
+        </ul>
+        <Route path='/' exact component={App}></Route>
+        <Route path='/team2' component={Team2}></Route>
+        <Route path='/team3' component={Team3}></Route>
+      </div>
+    </BrowserRouter>
   </Provider>),
   document.getElementById('root')
 );
